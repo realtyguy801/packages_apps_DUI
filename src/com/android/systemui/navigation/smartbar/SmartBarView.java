873282a801555617b10be43b5f14eafd80aa3831
ezio84 <brabus84@gmail.com>
2017-03-31 11:47:28 +0200

    SmartBar: allow set custom buttons opacity when Pulse is active [1/3]
    nowadays with navbar themes the user can use custom navbar buttons
    with particular design (e.g. an empty square with just corners),
    in those cases he could need to keep max opacity when pulse is
    active otherwise buttons visibility would be compromised.
    
    Change-Id: Ida82ebbed713e4dee3e61bb76a0696ad60ac86d4
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -77,8 +77,6 @@ import java.util.Set;
 public class SmartBarView extends BaseNavigationBar {
     final static boolean DEBUG = false;
     final static String TAG = SmartBarView.class.getSimpleName();
-    final static float PULSE_ALPHA_FADE = 0.3f; // take bar alpha low so keys are vaguely visible
-                                                // but not intrusive during Pulse
     final static int PULSE_FADE_OUT_DURATION = 250;
     final static int PULSE_FADE_IN_DURATION = 200;
 
@@ -117,7 +115,7 @@ public class SmartBarView extends BaseNavigationBar {
         sUris.add(Settings.System.getUriFor(Settings.System.DIAMOND_ANIMATION_DURATION));
         sUris.add(Settings.System.getUriFor(Settings.System.DOTS_RESIZE_DURATION));
         sUris.add(Settings.System.getUriFor(Settings.System.HOME_RESIZE_DURATION));
-
+        sUris.add(Settings.Secure.getUriFor(Settings.Secure.PULSE_CUSTOM_BUTTONS_OPACITY));
     }
 
     private SmartObservable mObservable = new SmartObservable() {
@@ -157,6 +155,8 @@ public class SmartBarView extends BaseNavigationBar {
                 setDotsAnimationDuration();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.HOME_RESIZE_DURATION))) {
                 setHomeResizeAnimationDuration();
+            } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.PULSE_CUSTOM_BUTTONS_OPACITY))) {
+                updatePulseNavButtonsOpacity();
             }
         }
     };
@@ -179,6 +179,7 @@ public class SmartBarView extends BaseNavigationBar {
     private static boolean mNavTintSwitch;
     public static int mIcontint;
     private static boolean mNavTintCustomIconSwitch;
+    public float mPulseNavButtonsOpacity;
 
     private GestureDetector mNavDoubleTapToSleep;
     private SlideTouchEvent mSlideTouchEvent;
@@ -607,6 +608,7 @@ public class SmartBarView extends BaseNavigationBar {
         setMenuVisibility(mShowMenu, true);
         setNavigationIconHints(mNavigationIconHints, true);
         setButtonAlpha();
+        updatePulseNavButtonsOpacity();
         setOpaLandscape(mVertical);
     }
 
@@ -830,6 +832,17 @@ public class SmartBarView extends BaseNavigationBar {
         }
     }
 
+    private void updatePulseNavButtonsOpacity() {
+        mPulseNavButtonsOpacity = alphaIntToFloat(Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                Settings.Secure.PULSE_CUSTOM_BUTTONS_OPACITY, 200, UserHandle.USER_CURRENT));
+        if (isBarPulseFaded()) {
+            final View currentNavButtons = getCurrentView().findViewWithTag(Res.Common.NAV_BUTTONS);
+            final View hiddenNavButtons = getHiddenView().findViewWithTag(Res.Common.NAV_BUTTONS);
+            currentNavButtons.setAlpha(mPulseNavButtonsOpacity);
+            hiddenNavButtons.setAlpha(mPulseNavButtonsOpacity);
+        }
+    }
+
     @Override
     public boolean onStartPulse(Animation animatePulseIn) {
         if (mEditor != null && mEditor.getMode() == BaseEditor.MODE_ON) {
@@ -837,7 +850,7 @@ public class SmartBarView extends BaseNavigationBar {
         }
         final View currentNavButtons = getCurrentView().findViewWithTag(Res.Common.NAV_BUTTONS);
         final View hiddenNavButtons = getHiddenView().findViewWithTag(Res.Common.NAV_BUTTONS);
-        final float fadeAlpha = Math.min(mCustomAlpha, PULSE_ALPHA_FADE);
+        final float fadeAlpha = mPulseNavButtonsOpacity;
 
         // no need to animate the GONE view, but keep alpha inline since onStartPulse
         // is a oneshot call
